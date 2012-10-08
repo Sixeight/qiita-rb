5bc95152ff07396fd3bd7baed47f2ef982fd3ecf
Tomohiro Nishimura <tomohiro68@gmail.com>
2012-10-08 21:48:13 +0900

    Fix Qiita.new was called twice problem

--- lib/qiita.rb
+++ lib/qiita.rb
@@ -9,8 +9,13 @@ module Qiita
 
     # Delegate to Qiita::Client.new
     def method_missing(method, *args, &block)
-      return super unless new.respond_to?(method)
+      return super unless respond_to?(method)
       new.send(method, *args, &block)
+    end
+
+    def respond_to_missing?(method, include_private = false)
+      Qiita::Client.public_method_defined?(method)
     end
   end
 end
+
